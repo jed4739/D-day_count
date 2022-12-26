644ca6fa5681e59aac3857511a02b309a3ec8401
jed4739 <tjsdlr04@gmail.com>
2022-12-26 16:33:51 +0900

    음력 시도 2022.12.26 -1

--- app/src/main/java/com/example/d_day_count/MainActivity.java
+++ app/src/main/java/com/example/d_day_count/MainActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.DatePickerDialog;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.d_day_count.databinding.Main;
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     Calendar calendar_start, calendar_end, birth;
 
     boolean dateChecker;
+    boolean gregorianChecker = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +41,57 @@ public class MainActivity extends AppCompatActivity {
         calendar_end = Calendar.getInstance();
         birth = Calendar.getInstance();
 
+
         birth.set(2004,5, 5);
         String birthCount = "+" + TimeUnit.MILLISECONDS.toDays(calendar_start.getTimeInMillis()-birth.getTimeInMillis());
         binding.birthCount.setText(birthCount);
 
         binding.startBtn.setOnClickListener(v -> {
             Log.i("ButtonChecker", "ButtonCheck StartBtn");
-            datePickerDialog(true);
+            if (gregorianChecker) {
+                datePickerDialog(true);
+            } else {
+                lunarDatePickerDialog(true);
+            }
         });
         binding.endBtn.setOnClickListener(v -> {
             Log.i("ButtonChecker", "ButtonCheck EndBtn");
-            datePickerDialog(false);
+            if (gregorianChecker) {
+                datePickerDialog(false);
+            } else {
+                lunarDatePickerDialog(false);
+            }
         });
+        binding.calendarBtn.setOnClickListener(v -> {
+            Log.i("ButtonChecker", "ButtonCheck CalendarBtn");
+            if (dateChecker && binding.calendarBtn.getText() == "양력 달력") {
+                Toast("현재 양력달력이 설정되어 있습니다.\n초기화를 진행해주세요.");
+            } else if (dateChecker && binding.calendarBtn.getText() == "음력 달력") {
+                Toast("현재 음력달력이 설정되어 있습니다.\n초기화를 진행해주세요.");
+            } else {
+                changeCalendar();
+            }
+        });
+    }
+
+    private void changeCalendar() {
+        Button calendarBtn = binding.calendarBtn;
+        if (calendarBtn.getText() == "양력 달력") {
+            calendarBtn.setText("음력 달력");
+            Toast("음력 달력으로 변경되었습니다.");
+            gregorianChecker = false;
+        } else {
+            calendarBtn.setText("양력 달력");
+            Toast("양력 달력으로 변경되었습니다.");
+            gregorianChecker = true;
+        }
     }
     /*
     * btnChecker = 시작일과 마지막일을 구분하는 변수
     * year, month, day = 선택한 날짜의 값
     * startDate, endDate = 로깅 변수
     * */
-    public void datePickerDialog(boolean btnChecker) {
+    private void datePickerDialog(boolean btnChecker) {
         GregorianCalendar today = new GregorianCalendar();
         int year = today.get(Calendar.YEAR);
         int month = today.get(Calendar.MONTH);
@@ -66,14 +99,14 @@ public class MainActivity extends AppCompatActivity {
         DatePickerDialog dlg = new DatePickerDialog(this, (view, year_num, month_num, dayOfMonth) -> {
             if (btnChecker) {
                 startDate = year_num + "년 " + (month_num + 1) + "월 " + dayOfMonth + "일";
-                Toast.makeText(getApplicationContext(), "시작일은 " + startDate + " 입니다.", Toast.LENGTH_LONG).show();
+                Toast("시작일은 " + startDate + " 입니다.");
 
                 calendar_start.set(year_num, month_num + 1, dayOfMonth);
                 dateChecker = true;
             } else {
                 if (dateChecker) {
                     endDate = year_num + "년 " + (month_num + 1) + "월 " + dayOfMonth + "일";
-                    Toast.makeText(getApplicationContext(), "종료일은 " + endDate + " 입니다.", Toast.LENGTH_LONG).show();
+                    Toast("종료일은 " + endDate + " 입니다.");
 
                     calendar_end.set(year_num, month_num + 1, dayOfMonth);
                     long finalDate = TimeUnit.MILLISECONDS.toDays((calendar_end.getTimeInMillis() - calendar_start.getTimeInMillis()));
@@ -84,11 +117,48 @@ public class MainActivity extends AppCompatActivity {
                         binding.finalDate.setText(String.valueOf(finalDate));
                     }
                 } else {
-                    Toast.makeText(getApplicationContext(), "시작일을 설정해주세요!", Toast.LENGTH_SHORT).show();
+                    Toast("시작일을 설정해주세요!");
                 }
             }
         },year,month,day);
         dlg.show();
+    }
+
+    private void lunarDatePickerDialog(boolean lunar) {
+        GregorianCalendar today = new GregorianCalendar();
+        int year = today.get(Calendar.YEAR);
+        int month = today.get(Calendar.MONTH);
+        int day = today.get(Calendar.DATE);
+        DatePickerDialog dlg = new DatePickerDialog(this, (view, year_num, month_num, dayOfMonth) -> {
+            if (lunar) {
+                startDate = year_num + "년 " + (month_num + 1) + "월 " + dayOfMonth + "일";
+                Toast("시작일은 " + startDate + " 입니다.");
+
+                calendar_start.set(year_num, month_num + 1, dayOfMonth);
+                dateChecker = true;
+            } else {
+                if (dateChecker) {
+                    endDate = year_num + "년 " + (month_num + 1) + "월 " + dayOfMonth + "일";
+                    Toast("종료일은 " + endDate + " 입니다.");
+
+                    calendar_end.set(year_num, month_num + 1, dayOfMonth);
+                    long finalDate = TimeUnit.MILLISECONDS.toDays((calendar_end.getTimeInMillis() - calendar_start.getTimeInMillis()));
+                    if (finalDate < 0) {
+                        long i = finalDate * -1;
+                        binding.finalDate.setText(String.valueOf(i));
+                    } else {
+                        binding.finalDate.setText(String.valueOf(finalDate));
+                    }
+                } else {
+                    Toast("시작일을 설정해주세요!");
+                }
+            }
+        },year,month,day);
+        dlg.show();
+    }
+
+    private void Toast(String toast) {
+        Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
     }
 
 }
